9d74d4e14351428312c7f312599d363264233eed
Jonas Keinholz <git@inyono.dev>
2020-04-15 15:28:39 +0200

    update serlo.org

--- serlo-org.tf
+++ serlo-org.tf
@@ -2,9 +2,9 @@ locals {
   serlo_org = {
     image_tags = {
       server = {
-        httpd             = "11.1.2"
-        php               = "11.1.2"
-        migrate           = "11.1.2"
+        httpd             = "11.1.3"
+        php               = "11.1.3"
+        migrate           = "11.1.3"
         notifications_job = "2.1.0"
       }
       editor_renderer        = "8.1.0"
