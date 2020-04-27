d6ff9d55794b69c6c78cdb0661ef543cca3d6661
Jonas Keinholz <git@inyono.dev>
2020-04-27 12:51:03 +0200

    update api

--- api.tf
+++ api.tf
@@ -1,7 +1,15 @@
 locals {
   api = {
-    image_tag = "0.4.3"
+    image_tag = "0.5.0"
   }
+}
+
+module "api_redis" {
+  source = "github.com/serlo/infrastructure-modules-shared.git//redis?ref=dc5d73bc3919af66e281f6be6ba1301f94bca936"
+
+  namespace     = kubernetes_namespace.api_namespace.metadata.0.name
+  chart_version = "10.5.7"
+  image_tag     = "4.0.14"
 }
 
 module "api_secrets" {
@@ -9,13 +17,14 @@ module "api_secrets" {
 }
 
 module "api_server" {
-  source = "github.com/serlo/infrastructure-modules-api.git//server?ref=e0c3b128bdeb371efabc891f118fd4e6bd023088"
+  source = "github.com/serlo/infrastructure-modules-api.git//server?ref=37ce1a064979b768b0e248bbf32adcb114237b5d"
 
   namespace         = kubernetes_namespace.api_namespace.metadata.0.name
   image_tag         = local.api.image_tag
   image_pull_policy = "IfNotPresent"
 
   secrets              = module.api_secrets
+  redis_host           = "redis-master"
   serlo_org_ip_address = module.serlo_org.server_service_ip_address
 }
 
