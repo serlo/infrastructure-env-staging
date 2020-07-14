a93628e70f98c3dcd97349af55a55e05a0829226
Jonas Keinholz <git@inyono.dev>
2020-07-14 03:37:53 +0200

    updates

--- api.tf
+++ api.tf
@@ -1,6 +1,6 @@
 locals {
   api = {
-    image_tag = "0.5.5"
+    image_tag = "0.5.6"
   }
 }
 
--- serlo-org.tf
+++ serlo-org.tf
@@ -2,9 +2,9 @@ locals {
   serlo_org = {
     image_tags = {
       server = {
-        httpd             = "13.6.0"
-        php               = "13.6.0"
-        migrate           = "13.6.0"
+        httpd             = "13.6.1"
+        php               = "13.6.1"
+        migrate           = "13.6.1"
         notifications_job = "2.1.0"
       }
       editor_renderer        = "9.0.1"
