5b40dd60261a01b68148ca188af5514976edbcda
Jonas Keinholz <git@inyono.dev>
2020-09-22 13:42:04 +0200

    updates

--- providers.tf
+++ providers.tf
@@ -4,13 +4,13 @@ provider "cloudflare" {
 }
 
 provider "google" {
-  version     = "3.39.0"
+  version     = "3.40.0"
   project     = local.project
   credentials = file(local.credentials_path)
 }
 
 provider "google-beta" {
-  version     = "3.39.0"
+  version     = "3.40.0"
   project     = local.project
   credentials = file(local.credentials_path)
 }
