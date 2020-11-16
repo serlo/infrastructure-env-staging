372ed80edc8295b5cef49db47c6a266f0c519101
Jonas Keinholz <git@inyono.dev>
2020-11-16 03:15:47 +0100

    move provider versions into required_providers

--- providers.tf
+++ providers.tf
@@ -1,22 +1,18 @@
 provider "cloudflare" {
-  version   = "2.13.2"
   api_token = var.cloudflare_token
 }
 
 provider "google" {
-  version     = "3.47.0"
   project     = local.project
   credentials = file(local.credentials_path)
 }
 
 provider "google-beta" {
-  version     = "3.47.0"
   project     = local.project
   credentials = file(local.credentials_path)
 }
 
 provider "helm" {
-  version = "1.3.2"
   kubernetes {
     host     = module.cluster.endpoint
     username = ""
@@ -29,27 +25,10 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  version          = "1.13.2"
   host             = module.cluster.endpoint
   load_config_file = false
 
   client_certificate     = base64decode(module.cluster.auth.client_certificate)
   client_key             = base64decode(module.cluster.auth.client_key)
   cluster_ca_certificate = base64decode(module.cluster.auth.cluster_ca_certificate)
-}
-
-provider "null" {
-  version = "2.1.2"
-}
-
-provider "random" {
-  version = "2.2.1"
-}
-
-provider "template" {
-  version = "2.1.2"
-}
-
-provider "tls" {
-  version = "2.1.1"
 }
--- versions.tf
+++ versions.tf
@@ -2,31 +2,40 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = "2.13.2"
     }
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = "3.47.0"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
+      version = "3.47.0"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = "1.3.2"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "1.13.2"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
+      version = "2.1.2"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = "2.2.1"
     }
     template = {
-      source = "hashicorp/template"
+      source  = "hashicorp/template"
+      version = "2.1.2"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
+      version = "2.1.1"
     }
   }
 }
