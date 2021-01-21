728e03020461d7c900be58234ecb9b4f36defcf6
Jonas Keinholz <git@inyono.dev>
2021-01-21 01:40:21 +0100

    update api

--- api.tf
+++ api.tf
@@ -16,7 +16,7 @@ module "api_redis" {
 }
 
 module "api" {
-  source = "github.com/serlo/infrastructure-modules-api.git//?ref=v4.1.1"
+  source = "github.com/serlo/infrastructure-modules-api.git//?ref=v4.1.2"
 
   namespace         = kubernetes_namespace.api_namespace.metadata.0.name
   image_tag         = local.api.image_tags.server
