6277aeaa54295df944368e90a89523a22c3b5711
Stephan Kulla <git.mail@kulla.me>
2021-08-10 15:03:07 +0200

    update legacy serlo.org

--- serlo-org.tf
+++ serlo-org.tf
@@ -2,9 +2,9 @@ locals {
   serlo_org = {
     image_tags = {
       server = {
-        httpd             = "18.0.11"
-        php               = "18.0.11"
-        migrate           = "18.0.11"
+        httpd             = "18.0.12"
+        php               = "18.0.12"
+        migrate           = "18.0.12"
         notifications_job = "3.0.5"
       }
       editor_renderer        = "13.1.1"
