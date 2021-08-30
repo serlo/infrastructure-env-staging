a93dea2bca08d3a707445203d896498551b3ce09
Stephan Kulla <git.mail@kulla.me>
2021-08-30 12:08:37 +0200

    update api

--- api.tf
+++ api.tf
@@ -2,7 +2,7 @@ locals {
   api = {
     image_tags = {
       database_layer = "0.3.22"
-      server         = "0.26.22"
+      server         = "0.26.23"
       cache_worker   = "0.4.2"
     }
   }
