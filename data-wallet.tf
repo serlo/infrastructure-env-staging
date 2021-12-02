2c12162da2b293aa33b13a1ecde8ae28a13eb453
Hugo Tiburtino <hugotiburtino@protonmail.com>
2021-12-03 00:59:29 +0200

    refactor(enmeshed): rename module to data-wallet and handle ingress

--- data-wallet.tf
+++ data-wallet.tf
@@ -1,5 +1,5 @@
 locals {
-  enmeshed = {
+  data_wallet = {
     image_tags = {
       enmeshed = "2.1.1"
       mongodb  = "4.4.8"
@@ -7,16 +7,32 @@ locals {
   }
 }
 
-module "enmeshed" {
+module "data_wallet" {
   source = "../infrastructure-modules-shared/enmeshed" # TODO
 
   namespace  = kubernetes_namespace.enmeshed_namespace.metadata.0.name
-  image_tags = local.enmeshed.image_tags
+  image_tags = local.data_wallet.image_tags
   host       = "enmeshed.${local.domain}"
-}
 
+
+}
+# TODO: maybe change it to data_wallet_namespace
 resource "kubernetes_namespace" "enmeshed_namespace" {
   metadata {
+    # TODO: maybe change it to data-wallet
     name = "enmeshed"
   }
+}
+
+module "enmeshed_ingress" {
+  source = "github.com/serlo/infrastructure-modules-shared.git//ingress?ref=v6.0.0"
+
+  name      = "enmeshed"
+  namespace = kubernetes_namespace.enmeshed_namespace.metadata.0.name
+  host      = "enmeshed.${local.domain}"
+  backend = {
+    service_name = module.data_wallet.enmeshed_connector_service_name
+    service_port = module.data_wallet.enmeshed_connector_service_port
+  }
+  enable_tls = true
 }
