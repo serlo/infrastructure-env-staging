b38563e61294b2e57ad3ac7ad686996653e155d7
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-06-10 00:21:57 +0200

    feat: add kratos module
    based on 4fcf23b with some updates

--- auth.tf
+++ auth.tf
@@ -1,8 +1,17 @@
 locals {
+  # TODO: not sure if it is a good idea to update hydra and kratos at the same time
+  # in v0.23.2 the admin container takes long to be ready, which leads to a timeout in terraform
+  # and terraform thinks it is tainted
+  ory_chart_version = "0.23.2"
+
   hydra = {
-    chart_version = "0.23.2"
-    image_tag     = "v1.11.8"
+    image_tag = "v1.11.8"
   }
+
+  kratos = {
+    image_tag = "v0.9.0-alpha.2"
+  }
+
   keycloak = {
     chart_version = "5.2.0"
     image_tag     = "14.0.0"
@@ -13,7 +22,7 @@ module "hydra" {
   source = "github.com/serlo/infrastructure-modules-shared.git//hydra?ref=v12.0.0"
 
   namespace     = kubernetes_namespace.hydra_namespace.metadata.0.name
-  chart_version = local.hydra.chart_version
+  chart_version = local.ory_chart_version
   image_tag     = local.hydra.image_tag
   node_pool     = module.cluster.node_pools.preemptible
 
@@ -23,6 +32,20 @@ module "hydra" {
   url_logout  = "https://de.${local.domain}/auth/hydra/logout"
   url_consent = "https://de.${local.domain}/auth/hydra/consent"
   host        = "hydra.${local.domain}"
+}
+
+module "kratos" {
+  # TODO: use a tag when released
+  source = "github.com/serlo/infrastructure-modules-shared.git//kratos?ref=kratos"
+
+  namespace = kubernetes_namespace.kratos_namespace.metadata.0.name
+  # TODO: add extra user for kratos
+  dsn  = "postgres://${module.kpi.kpi_database_username_default}:${var.kpi_kpi_database_password_default}@${module.gcloud_postgres.database_private_ip_address}/kratos"
+  host = "kratos.${local.domain}"
+  # TODO: rename
+  smtp_password = var.athene2_php_smtp_password
+  chart_version = local.ory_chart_version
+  image_tag     = local.kratos.image_tag
 }
 
 module "keycloak" {
@@ -43,9 +66,16 @@ module "keycloak" {
   }
 }
 
+
 resource "kubernetes_namespace" "hydra_namespace" {
   metadata {
     name = "hydra"
+  }
+}
+
+resource "kubernetes_namespace" "kratos_namespace" {
+  metadata {
+    name = "kratos"
   }
 }
 
