5cba547f75e33cc0892ef5c27888f368d8fc1c4a
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-07-08 00:00:24 +0200

    Upgrade db layer

--- api.tf
+++ api.tf
@@ -1,8 +1,7 @@
 locals {
   api = {
     image_tags = {
-      # TODO: upgrade to 0.3.52, UserCreateMutation
-      database_layer = "0.3.51"
+      database_layer = "0.3.52"
       server         = "0.43.0-staging.1"
       cache_worker   = "0.4.2"
     }
