b25edd19ffde96bc7ce0314293add0aa9167853a
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-09-23 21:41:47 +0200

    Upgrade api and auth modules after clean up

--- api.tf
+++ api.tf
@@ -18,7 +18,7 @@ module "api_redis" {
 }
 
 module "api" {
-  source = "github.com/serlo/infrastructure-modules-api.git//?ref=v9.2.0"
+  source = "github.com/serlo/infrastructure-modules-api.git//?ref=v10.0.0"
 
   namespace         = kubernetes_namespace.api_namespace.metadata.0.name
   image_tag         = local.api.image_tags.server
