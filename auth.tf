2a406f1d0a485bc8854cd106df1c7ab880fc5716
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-11-17 08:31:27 +0100

    refactor(hydra): use usual domain, not custom one

--- auth.tf
+++ auth.tf
@@ -20,9 +20,9 @@ module "hydra" {
 
   # TODO: add extra user for hydra
   dsn         = "postgres://${module.kpi.kpi_database_username_default}:${var.kpi_kpi_database_password_default}@${module.gcloud_postgres.database_private_ip_address}/hydra"
-  url_login   = "https://kratos-vercel.serlo-staging.dev/auth/oauth/login"
-  url_logout  = "https://kratos-vercel.serlo-staging.dev/auth/oauth/logout"
-  url_consent = "https://kratos-vercel.serlo-staging.dev/auth/oauth/consent"
+  url_login   = "https://${local.domain}/auth/oauth/login"
+  url_logout  = "https://${local.domain}/auth/oauth/logout"
+  url_consent = "https://${local.domain}/auth/oauth/consent"
   host        = "hydra.${local.domain}"
 }
 
