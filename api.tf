c24e915be061b0341e1a7b9088b3df6a7d70300e
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-11-17 08:30:32 +0100

    Merge main into kratos branch

--- api.tf
+++ api.tf
@@ -1,7 +1,7 @@
 locals {
   api = {
     image_tags = {
-      database_layer = "0.3.54"
+      database_layer = "0.3.56"
       server         = "0.47.0-staging.13"
       cache_worker   = "0.4.2"
     }
