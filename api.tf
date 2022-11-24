9b82c9a2e8977d8d294567d635731421d86f5637
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-11-24 23:52:32 +0100

    kratos: upgrade api to solve problem with language trait

--- api.tf
+++ api.tf
@@ -2,7 +2,7 @@ locals {
   api = {
     image_tags = {
       database_layer = "0.3.57"
-      server         = "0.48.0-staging.0"
+      server         = "0.48.0-staging.1"
       cache_worker   = "0.4.2"
     }
   }
