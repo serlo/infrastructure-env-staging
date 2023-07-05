64968d9466878c6a10383cd77c23bc1424cf3d43
eliflores <eli@serlo.org>
2023-07-05 12:23:05 +0200

    Deploy version 0.52.1 of the server (api)
    The tag update was already applied via `terraform apply` ✨

--- api.tf
+++ api.tf
@@ -2,7 +2,7 @@ locals {
   api = {
     image_tags = {
       database_layer   = "0.3.66"
-      server           = "0.52.0"
+      server           = "0.52.1"
       cache_worker     = "0.4.2"
       api_db_migration = "0.1.0"
     }
