ae759ff97daa4d156cd93b101b559407f397f506
Stephan Kulla <git.mail@kulla.me>
2023-08-28 10:16:59 +0200

    update db layer

--- api.tf
+++ api.tf
@@ -1,7 +1,7 @@
 locals {
   api = {
     image_tags = {
-      database_layer   = "0.3.69-staging.1"
+      database_layer   = "0.3.69"
       server           = "0.54.0"
       api_db_migration = "0.2.0-staging.5"
     }
