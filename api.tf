0ff7af4bfd041ff64e0f4d35dfe81191fd3027e9
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-10-31 15:30:55 +0100

    Upgrade api image to improve enmeshed endpoint

--- api.tf
+++ api.tf
@@ -2,7 +2,7 @@ locals {
   api = {
     image_tags = {
       database_layer             = "0.3.72"
-      server                     = "0.57.7-staging.0"
+      server                     = "0.57.7-staging.1"
       api_db_migration           = "0.6.0-staging"
       content_generation_service = "0.2.4"
     }
