dbb70ef75fc167d6d88e539dae4b73b3a8297bfb
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-11-10 12:07:59 +0100

    Upgrade api module to destroy db layer swr

--- api.tf
+++ api.tf
@@ -19,7 +19,7 @@ module "api_redis" {
 }
 
 module "api" {
-  source = "github.com/serlo/infrastructure-modules-api.git//?ref=v12.3.1"
+  source = "github.com/serlo/infrastructure-modules-api.git//?ref=v13.0.0"
 
   namespace         = kubernetes_namespace.api_namespace.metadata.0.name
   image_tag         = local.api.image_tags.server
