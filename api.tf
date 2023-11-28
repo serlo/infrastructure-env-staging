aa821c7b4bafbe30c9b9c17f87d2a40fe3653fc7
Stephan Kulla <git.mail@kulla.me>
2023-11-28 10:26:06 +0100

    update db migration

--- api.tf
+++ api.tf
@@ -3,7 +3,7 @@ locals {
     image_tags = {
       database_layer   = "0.3.74"
       server           = "0.58.1"
-      api_db_migration = "0.8.0-staging.0"
+      api_db_migration = "0.8.0-staging.1"
     }
   }
 }
